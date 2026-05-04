bba9e7fdd79c733219fc1fd59f2c6efe2352f935
Pedriprog <apetracca@studenti.apuliadigital.it>
2026-05-04 10:38:52 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/workshop-form-frontend/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
